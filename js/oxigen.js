736eb05673a2f7bdc795d434b3a5765dbba043d1
BuildTools <unconfigured@null.spigotmc.org>
2018-01-26 15:36:05 -0300

    frases aleatorias, texto nuevo

--- js/oxigen.js
+++ js/oxigen.js
@@ -12,11 +12,11 @@ $("#menu ul").click(function() {
 //Frase
 
 var fr = [
-  '“Lo importante no es tener muchas ideas, sino la idea oportuna en cada caso.”',
-  '“Las ideas son como las pulgas, saltan de un hombre a otro. Pero no pican a todo el mundo.”',
+  '“Piensa diferente, piensa OxigenArts.”',
+  '“OxigenArts hace que tus sueños se hagan realidad.”',
   '“Las grandes ideas son aquellas de las que lo único que nos sorprende es que no se nos hayan ocurrido antes.”',
-  "“Como fuerza social, un individuo con una idea vale por noventa y nueve con un solo interés.”",
-  '“Las que conducen y arrastran al mundo no son las máquinas, sino las ideas.”'
+  "“No te preocupes, OxigenArts te lo tiene.”",
+  '“OxigenArts, el mejor servicio en todo.”'
 ]
 
 var frase = fr[Math.floor(Math.random()*fr.length)];
