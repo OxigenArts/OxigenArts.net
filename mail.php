db53648ea76d6f75a08c14398a0126aa8a933473
root <root@oxigenarts.net>
2018-02-03 21:06:53 +0000

    mail php

--- mail.php
+++ mail.php
@@ -1,7 +1,10 @@
 <?php
-$to =$POST['email'];
-$subject = 'Mensaje de la web';
-$message = "Nombre: $POST['name']\r\nEmail: $POST['email']\r\nMensaje: $POST['msg']\r\n";
+$name =  $POST['name'];
+$email = $POST['email'];
+$msg = $POST['msg'];
+$to = "contact@oxigenarts.net";
+$subject = "Mensaje de la web";
+$message = "Nombre: $name \r\nEmail: $email \r\nMensaje: $msg \r\n";
 $headers = "From: contact@oxigenarts.net\r\nReply-To: contact@oxigenarts.net \r\n X-Mailer: PHP/". phpversion();
 mail($to, $subject, $message, $headers);
 ?>
